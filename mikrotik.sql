d8217a03bf1d24f8112316a4de77e7e2df3ea655
masrois46 <masrois46@gmail.com>
2018-07-27 18:33:43 +0700

    Update

--- mikrotik.sql
+++ mikrotik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Waktu pembuatan: 27 Jul 2018 pada 10.39
+-- Waktu pembuatan: 27 Jul 2018 pada 11.32
 -- Versi server: 5.7.21
 -- Versi PHP: 5.6.35
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `mikrotik_manajemen`
+-- Database: `mikrotik`
 --
 
 -- --------------------------------------------------------
@@ -35,13 +35,6 @@ CREATE TABLE IF NOT EXISTS `admin` (
   `nama` varchar(150) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `admin`
---
-
-INSERT INTO `admin` (`username`, `password`, `nama`) VALUES
-('admin', 'c3284d0f94606de1fd2af172aba15bf3', '');
 
 -- --------------------------------------------------------
 
@@ -61,9 +54,9 @@ CREATE TABLE IF NOT EXISTS `config` (
 --
 
 INSERT INTO `config` (`key`, `value`) VALUES
-('MIKROTIK_HOST', '192.168.1.2'),
-('MIKROTIK_PASS', '01012012'),
-('MIKROTIK_USER', 'admin');
+('MIKROTIK_HOST', ''),
+('MIKROTIK_PASS', ''),
+('MIKROTIK_USER', '');
 
 -- --------------------------------------------------------
 
@@ -80,13 +73,6 @@ CREATE TABLE IF NOT EXISTS `user` (
   `chart` date NOT NULL,
   PRIMARY KEY (`user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`user`, `type`, `registered`, `expired`, `chart`) VALUES
-('kdaforex', 'hotspot', 1530403200, 1532995200, '2018-07-01');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
